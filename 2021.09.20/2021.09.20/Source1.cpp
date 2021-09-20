f6347829e4f2bd28a2239b4a14b90c88b39105fb
st095236 <st095236@student.spbu.ru>
2021-09-20 19:18:15 +0300

    LESSON 1 HW

---
+++ 2021.09.20/2021.09.20/Source1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	
+	int b = 0;
+	
+	int c = 0;
+	
+	cin >> b >> c;
+	
+	a = b / c;
+	
+	cout << a;
+
+	return EXIT_SUCCESS;
+}
