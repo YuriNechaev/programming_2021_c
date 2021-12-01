d1b1469fa920564e587b0bbec38b516f5327c09d
YuriNechaev <st095236@student.spbu.ru>
2021-12-01 18:52:36 +0300

    Homework fix

---
+++ 2021.09.21_Homework_1_/Task7/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	int t = 0;
+
+	cout << "Input a:";
+	cin >> a;
+	cout << "Input b:";
+	cin >> b;
+
+	t = b;	b = a;	a = t;
+
+	cout << "a now is equal to " << a << endl;
+	cout << "b now is equal to " << b;
+
+	return EXIT_SUCCESS;
+}
