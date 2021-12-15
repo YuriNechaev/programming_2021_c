de53874f8bf004d83b5a05cdc547f71713e172b7
YuriNechaev <st095236@student.spbu.ru>
2021-12-15 20:03:30 +0300

    Homework fix

--- 2021.11.12_Homework_6/Task1/Source.cpp
+++ 2021.11.12_Homework_6/Task1/Source.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int c = 0;
+	int c = 10;
+	int t = 0;
 	int b = 0;
 	int newelem = 0;
-	int* arr = nullptr;
-	arr = new int[0]{ 0 };
+	int* arr = new int[c] { 0 };
+	bool exiting = false;
 
-	while (1)
+	while (!exiting)
 	{
 		cout << "Press 0 to exit the program" << endl
 			<< "Press 1 to show an array" << endl
@@ -21,20 +22,25 @@ int main(int argc, char* argv[])
 			<< "Press 6 to reverse an array" << endl;
 
 		cin >> b;
+
 		if (b == 0)
 		{
-			delete arr;
-			return EXIT_SUCCESS;
+			delete[] arr;
+			exiting = true;
 		}
-		if (b == 1)
+
+		else if (b == 1)
 		{
-			for (int i = 0; i <= c; ++i)
+			for (int i = 0; i < c; ++i)
 			{
-				cout << arr[i];
+				cout << arr[i] << " ";
 			}
+			cout << endl;
 		}
+
 		if (b == 2)
 		{
+			cout << "Input element:" << endl;
 			cin >> newelem;
 			int* newArr = new int[(c + 1) * sizeof(int)]{ 0 };
 			for (int i = 0; i < c; ++i)
@@ -42,15 +48,14 @@ int main(int argc, char* argv[])
 				newArr[i] = arr[i];
 			}
 			newArr[c] = newelem;
-			for (int i = 1; i < c + 1; ++i)
-			{
-				arr[i] = newArr[i];
-			}
+			delete[] arr;
+			arr = newArr;
 			c += 1;
-			delete newArr;
 		}
-		if (b == 3)
+
+		else if (b == 3)
 		{
+			cout << "Input element:" << endl;
 			cin >> newelem;
 			int* newArr = new int[(c + 1) * sizeof(int)]{ 0 };
 			for (int i = 0; i < c; ++i)
@@ -58,44 +63,45 @@ int main(int argc, char* argv[])
 				newArr[i + 1] = arr[i];
 			}
 			newArr[0] = newelem;
-			for (int i = 0; i < c + 1; ++i)
-			{
-				arr[i] = newArr[i];
-			}
+			delete[] arr;
+			arr = newArr;
 			c += 1;
-			delete newArr;
 		}
-		if (b == 4)
+
+		else if (b == 4)
 		{
 			int* newArr = new int[(c - 1) * sizeof(int)]{ 0 };
 			for (int i = 0; i < c - 1; ++i)
 			{
 				newArr[i] = arr[i];
 			}
-			for (int i = 0; i < c - 1; ++i)
-			{
-				arr[i] = newArr[i];
-			}
-			delete newArr;
+			delete[] arr;
+			arr = newArr;
 			c -= 1;
 		}
-		if (b == 5)
+
+		else if (b == 5)
 		{
 			int* newArr = new int[(c - 1) * sizeof(int)]{ 0 };
 			for (int i = 1; i < c; ++i)
 			{
 				newArr[i - 1] = arr[i];
 			}
-			for (int i = 0; i < c - 1; ++i)
-			{
-				arr[i] = newArr[i];
-			}
-			delete newArr;
+			delete[] arr;
+			arr = newArr;
 			c -= 1;
 		}
+
+		else if (b == 6)
+		{
+			for (int i = 0; i < c / 2; ++i)
+			{
+				t = arr[i];
+				arr[i] = arr[c - i - 1];
+				arr[c - i - 1] = t;
+			}
+		}
 	}
-
-
 
 	return EXIT_SUCCESS;
 }
